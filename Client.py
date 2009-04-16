be834a9cc49c17bdf883e2ab88b30f10aa26b311
frederik.pion <frederik.pion@f1e68b3a-21a6-11de-9e56-bf19dc6c588e>
2009-04-16 03:54:02 +0000

    Premier version du client elle permet de créer la connection et de créer le menu (Gui.py). Avec les autre classe, new project marche il reste a configurer l'envoi au serveur et la base de donner. load project marche, doit allez chercher le projet sur la bd. Import texte marche mais la méthode n'est pas encore fini

---
+++ Client.py
@@ -0,0 +1,48 @@
+#-*- coding: iso-8859-1 -*-
+import xmlrpclib
+from Gui import *
+from Projet import *
+
+class Client:
+    def __init__(self):
+        self.vue = Gui(self)
+        self.projetCourant = ""
+    def connect(self,serv):
+        self.serv = serv
+        self.numClient = self.serv.connect()
+        self.vue.mainMenu()
+        self.vue.root.mainloop()
+    def deconnect(self):
+        self.serv.deconnect(self.numClient)
+    def afficherMenu(self):
+        self.vueMenu.afficherMenu()
+    def newProject(self):
+        self.vue.initProjet()
+        self.vue.projetForm.tLevel.mainloop()
+    def createProject(self,nomProject):
+        self.projetCourant = Projet()
+        self.projetCourant.texte = ""
+        self.projetCourant.nomProjet = nomProject
+        #self.projetCourant.motClasses.verbe = ""
+        #self.projetCourant.motClasses.nom = ""
+        #self.projetCourant.motClasses.adjectif = ""
+        print self.nomProjet
+    def saveProject(self):
+        self.serv.save(self.projetCourant,self.nomProjet)
+    def loadProject(self):
+        self.projetCourant = self.serv.load(self.nomProjet)
+    def editAnalyseTextuelle(self):
+        self.form.analyse()      
+    def loadText(self,texte):
+        #self.form.loadText()
+        pass      
+
+if __name__ == "__main__":
+    s = xmlrpclib.ServerProxy('http://localhost:8006')
+    c = Client()
+    c.connect(s)
+    c.deconnect()
+    print c.numClient
+
+    
+
