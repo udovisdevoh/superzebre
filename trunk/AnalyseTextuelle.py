e87e8d01336e5d44396f6bb9e18cd85e07332cb9
frederik.pion <frederik.pion@f1e68b3a-21a6-11de-9e56-bf19dc6c588e>
2009-04-16 16:51:20 +0000

    Analyse Textuelle fait par Aladin qui permet de classer les mot et d'allez chercher l'object MotClasses par la suite

---
+++ trunk/AnalyseTextuelle.py
@@ -0,0 +1,25 @@
+#-*- coding: iso-8859-1 -*-
+from MotsClasses import *
+
+class AnalyseTextuelle(object):
+
+    def __init__(self):
+        self.vue = AnalyseTextuelleForm()
+        self.motsClasses = MotsClasses()
+
+    def getMotsClasses(self):
+        return motsClasses
+
+    def classerMot(self, mot, genre):     
+        if genre == 1:
+            motsClasses.ajouterNom(mot)
+        elif genre == 2:
+            motsClasses.ajouterVerbe(mot)
+        elif genre == 3:
+            motsClasses.ajouterAdjectif(mot)
+        else:
+            print "Erreur lors du classement du mot, genre erroné"
+            
+               
+if __name__ == "__main__":
+    analyse = AnalyseTextuelle()
