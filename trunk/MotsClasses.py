ee7e48acda32252d44e8a8e57f94b93e8573306c
frederik.pion <frederik.pion@f1e68b3a-21a6-11de-9e56-bf19dc6c588e>
2009-04-16 16:48:31 +0000

    nouvelle version de MotsClasses qui permet d'ajouter des nom,verbe,adjectif Update fais par Aladin

--- trunk/MotsClasses.py
+++ trunk/MotsClasses.py
@@ -1,7 +1,17 @@
 #-*- coding: iso-8859-1 -*-
 
 class MotsClasses(object):
+
     def __init__(self):
-        noms=[]
-        verbes=[]
-        adjectifs=[]
+        self.noms=[]
+        self.verbes=[]
+        self.adjectifs=[]
+
+    def ajouterNom(self, nom):
+        self.noms.append(nom)
+
+    def ajouterVerbe(self, ver):
+        self.verbes.append(ver)
+
+    def ajouterAdjectif(self, adj):
+        self.adjectifs.append(adj)
