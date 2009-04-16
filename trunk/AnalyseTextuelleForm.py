987c7ac84fd1de2c163a94c34f9189d9144fbe5d
frederik.pion <frederik.pion@f1e68b3a-21a6-11de-9e56-bf19dc6c588e>
2009-04-16 20:12:00 +0000

    nouvelle version de analyse form qui marche pour loader le texte et la séparation manuelle puis les rajoute a analyse textuelle reste juste a faire losque quon a fini loader les mot classes dans le projet courant

--- trunk/AnalyseTextuelleForm.py
+++ trunk/AnalyseTextuelleForm.py
@@ -7,46 +7,78 @@ class AnalyseTextuelleForm:
         self.master = parent.root
         self.master.title(title)
         self.frameTop = Frame(self.master)
-        self.frameBottom= Frame(self.master)
-        self.frameCentre = Frame(self.master)
+        self.frameBouton= Frame(self.master)
+        self.frameSaute = Frame(self.master)
+        self.frameBottom = Frame(self.master)
+        self.frameBoutonValide = Frame(self.master)
         
-        self.canvas = Canvas(self.frameBottom, width=800, height=400)
-        self.canvas.create_rectangle(4,4,266,400,width=2,fill="white")
-        self.canvas.create_rectangle(269,4,532,400,width=2,fill="white")
-        self.canvas.create_rectangle(535,4,800,400,width=2,fill="white")  
+        self.verbe = Text(self.frameBottom,height=25,width=30)
+        self.verbe.pack(side=LEFT)
+        self.verbebar = Scrollbar(self.frameBottom, orient=VERTICAL , command=self.verbe.yview)
+        self.verbebar.pack(side = LEFT , fill = Y)
+        self.verbe.configure(yscrollcommand = self.verbebar.set)
+        self.nom = Text(self.frameBottom,height=25,width=30)
+        self.nom.pack(side=LEFT)
+        self.nombar = Scrollbar(self.frameBottom, orient=VERTICAL , command=self.nom.yview)
+        self.nombar.pack(side = LEFT , fill = Y)
+        self.nom.configure(yscrollcommand = self.nombar.set)
+        self.adj = Text(self.frameBottom,height=25,width=30)
+        self.adj.pack(side=LEFT)
+        self.adjbar = Scrollbar(self.frameBottom, orient=VERTICAL , command=self.adj.yview)
+        self.adjbar.pack(side = LEFT , fill = Y)
+        self.adj.configure(yscrollcommand = self.adjbar.set)
+          
     
-        self.button = Button(self.frameBottom, text="QUIT", fg="red", command=self.frameBottom.quit)
-        self.button.pack(side=BOTTOM,anchor=E)
+        self.button = Button(self.frameBoutonValide, text="QUIT", fg="red", command=self.frameBottom.quit)
+        self.button.pack(side=RIGHT)
         
-        self.frameTop.pack(side=TOP,pady=50)
-        self.frameBottom.pack(side = BOTTOM)
-        self.frameCentre.pack()
+        self.frameTop.pack(side=TOP,pady=40)
+        self.frameSaute.pack(side = TOP)
+        self.frameBouton.pack(side = TOP,pady = 10)
+        self.frameBottom.pack(side = TOP,padx = 10)
+        self.frameBoutonValide.pack(side = TOP , pady = 15)
+        
          
-        self.bVerbe = Button(self.frameCentre,text="Verbes",width=20)
-        self.bNom = Button(self.frameCentre,text="Noms",width=20)
-        self.bAdj = Button(self.frameCentre,text="Adjectifs",width=20)
-        self.bSaute= Button(self.frameCentre,text="Sauter un mot",width=20)
+        self.bVerbe = Button(self.frameBouton,text="Verbes",width=36,command=self.addVerbe)
+        self.bNom = Button(self.frameBouton,text="Noms",width=36,command=self.addNom)
+        self.bAdj = Button(self.frameBouton,text="Adjectifs",width=36,command=self.addAdj)
+        self.bSaute= Button(self.frameSaute,text="Sauter un mot",width=36)
         
         self.texte = Text(self.frameTop,height=10)
         self.texte.insert(END, self.parent.parent.projetCourant.texte)
         self.texte.pack(side=LEFT)
         self.textebar = Scrollbar(self.frameTop, orient=VERTICAL , command=self.texte.yview)
         self.textebar.pack(side = LEFT , fill = Y)
-        self.texte.configure(yscrollcommand = self.textebar.set)
+        self.texte.configure(yscrollcommand = self.textebar.set,state=DISABLED)
            
-        self.bVerbe.pack(anchor=E,) 
-        self.bNom.pack(anchor=CENTER) 
-        self.bAdj.pack(anchor=W)
+        self.bVerbe.pack(side = LEFT,) 
+        self.bNom.pack(side = LEFT) 
+        self.bAdj.pack(side = LEFT)
         
-        self.bSaute.pack(side=BOTTOM,anchor=W,pady=20)
-        
-        
-        self.canvas.pack(side=BOTTOM,pady=20)  
-          
+        self.bSaute.pack(side=BOTTOM,anchor=W)
+    
+    def addVerbe(self):
+        verbe = self.texte.get("sel.first", "sel.last")
+        self.verbe.insert(END, verbe+"\n")
+        self.parent.parent.ajouterMots(verbe,2)
+    def addNom(self):
+        nom = self.texte.get("sel.first", "sel.last")
+        self.nom.insert(END, nom+"\n")
+        self.parent.parent.ajouterMots(nom,1)
+    def addAdj(self):
+        adj = self.texte.get("sel.first", "sel.last")
+        self.adj.insert(END, adj+"\n")
+        self.parent.parent.ajouterMots(adj,3)       
+
+
+
     
 if __name__=="__main__":
     from Client import *
     from Gui import *
     c = Client()
+    c.projetCourant=Projet()
+    c.projetCourant.texte = "kev stun gros debile"
+    c.vue.imgLabel.destroy()
     c.vue.test = AnalyseTextuelleForm(c.vue,"test")      
     c.vue.test.master.mainloop()
