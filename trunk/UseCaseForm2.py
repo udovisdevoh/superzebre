a2c12c792df2e284c512597e304cb9c549a62f73
melanconkevin <melanconkevin@f1e68b3a-21a6-11de-9e56-bf19dc6c588e>
2009-04-22 23:30:31 +0000

    Méthode apply à tester

--- trunk/UseCaseForm2.py
+++ trunk/UseCaseForm2.py
@@ -11,21 +11,21 @@ class UseCaseForm(object):
     def initGraphicsComponents(self, root, title):
         root.minsize(800, 600)
         root.maxsize(800, 600)
-        self.canvas = Canvas(root, width = 800, height = 600)
+        self.canvas = Canvas(root, width = 600, height = 400, bg = "blue")
         self.useCaseTitle = Text(self.canvas, width = 50, height = 1)
         self.useCaseDefinition = Text(self.canvas, height = 10)
         self.useCaseTitle.pack(side=TOP, padx = 25, pady = 10, anchor = W)
         self.useCaseDefinition.pack(side=TOP, padx = 25, pady = 5)
-        self.buttonPrevious = Button(self.canvas, text = "<<Précédent", command = self.previous)
-        self.buttonNext = Button(self.canvas, text = "Suivant>>", command = self.next)
-        self.buttonApply = Button(self.canvas, text = "Valider", command = self.apply)
-        self.buttonPrevious.pack(side = LEFT)
-        self.buttonNext.pack(side = LEFT)
-        self.buttonApply.pack(side = LEFT)
-        self.canvas.pack()
+        self.buttonPrevious = Button(self.canvas, text = "<<Précédent", bd = 5, command = self.previous)
+        self.buttonNext = Button(self.canvas, text = "Suivant>>", bd = 5, command = self.next)
+        self.buttonApply = Button(self.canvas, text = "Valider", bd = 5, command = self.apply)
+        self.buttonPrevious.pack(side = LEFT, padx = 25, ipadx = 16, ipady = 5)
+        self.buttonNext.pack(side = LEFT, padx = 25, ipadx = 20, ipady = 5)
+        self.buttonApply.pack(side = RIGHT, padx = 25, ipadx = 30, ipady = 5)
+        self.canvas.pack(anchor = CENTER, padx = 100, pady = 100)
     
     def previous(self):
-        if self.index != 0 and self.index != -1:
+        if self.index <= 0:
             self.index -= 1
             self.updateText()
     
@@ -46,7 +46,10 @@ class UseCaseForm(object):
         self.useCaseDefinition.insert(1.0, self.tabDefinition[self.index])
         
     def apply(self):
-        pass
+        self.tab = {}
+        for i in range(len(self.tabTitle)+1):
+            self.tab[self.tabTitle[i]] = self.tabDefinition[i]
+        return self.tab
     
 if __name__ == "__main__":
     root = Tk()
