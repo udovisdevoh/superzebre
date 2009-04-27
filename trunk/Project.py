b8fa4a5cb12973802cfbe814d02734e00a453441
frederik.pion <frederik.pion@f1e68b3a-21a6-11de-9e56-bf19dc6c588e>
2009-04-27 13:46:23 +0000

    Version du projet qui incorpore les cas d'usage

--- trunk/Project.py
+++ trunk/Project.py
@@ -4,4 +4,6 @@ class Project:
     def __init__(self,name):
         self.name = name
         self.sortedWords = SortedWords()
+        self.useCase = UseCaseList()
         self.text = None
+        
