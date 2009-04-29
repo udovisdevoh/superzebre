9601af049386ebd10a9eceb92e4a54e01bcb35bc
melanconkevin <melanconkevin@f1e68b3a-21a6-11de-9e56-bf19dc6c588e>
2009-04-29 16:25:29 +0000

    Yéééééé ça marche!

--- trunk/UseCaseForm.py
+++ trunk/UseCaseForm.py
@@ -1,17 +1,16 @@
 #-*- coding: iso-8859-1 -*-
 from Tkinter import *
-from UseCase import *
-from UseCaseList import *
 
 class UseCaseForm(object):
-    def __init__(self, root, title,useCaseList):
+    def __init__(self, root, title):
         self.initGraphicsComponents(root, title)
-        self.useCaseTab = useCaseList
-        self.index=len(self.useCaseTab)
-        
-
+        self.tabTitle = []
+        self.tabDefinition = []
+        self.index = 0
+    
     def initGraphicsComponents(self, root, title):
         root.minsize(800, 600)
+        root.maxsize(800, 600)
         self.canvas = Canvas(root, width = 600, height = 400, bg = "blue")
         self.useCaseTitle = Text(self.canvas, width = 50, height = 1)
         self.useCaseDefinition = Text(self.canvas, height = 10)
@@ -32,18 +31,18 @@ class UseCaseForm(object):
     
     def next(self):
         if self.useCaseTitle.get(1.0,END)!="\n":
-            if self.index == len(self.useCaseTab):
-                useCase = UseCase()
-                useCase.name = (self.useCaseTitle.get(1.0, END))[:-1]
-                useCase.description = (self.useCaseDefinition.get(1.0, END))[:-1]
-                self.useCaseTab.append(useCase)        
+            if self.index == len(self.tabTitle):
+                text = self.useCaseTitle.get(1.0, END)
+                self.tabTitle.append(text[:-1])
+                text =self.useCaseDefinition.get(1.0, END) 
+                self.tabDefinition.append(text[:-1])        
             else:
-                title = self.useCaseTitle.get(1.0, END)
-                definition =self.useCaseDefinition.get(1.0, END)
-                self.useCaseTab[self.index].name = title[:-1]
-                self.useCaseTab[self.index].description = definition[:-1]
+                text = self.useCaseTitle.get(1.0, END)
+                self.tabTitle[self.index] = text[:-1]
+                text =self.useCaseDefinition.get(1.0, END)
+                self.tabDefinition[self.index] = text[:-1]
             self.index += 1
-            if self.index == len(self.useCaseTab):
+            if self.index == len(self.tabTitle):
                 self.useCaseTitle.delete(1.0, END)
                 self.useCaseDefinition.delete(1.0, END)
             else:
@@ -55,15 +54,17 @@ class UseCaseForm(object):
     def updateText(self):
         self.useCaseTitle.delete(1.0, END)
         self.useCaseDefinition.delete(1.0, END)
-        self.useCaseTitle.insert(1.0, self.useCaseTab[self.index].name)
-        self.useCaseDefinition.insert(1.0, self.useCaseTab[self.index].description)
+        self.useCaseTitle.insert(1.0, self.tabTitle[self.index])
+        self.useCaseDefinition.insert(1.0, self.tabDefinition[self.index])
         
     def apply(self):
-        self.canvas.destroy()
-        
+        self.tab = {}
+        for i in range(len(self.tabTitle)+1):
+            self.tab[self.tabTitle[i]] = self.tabDefinition[i]
+        return self.tab
+    
 if __name__ == "__main__":
     root = Tk()
-    tab=[UseCase()]
-    useCaseForm = UseCaseForm(root, "test",tab)
+    useCaseForm = UseCaseForm(root, "test")
     useCaseForm.top = Toplevel(root)
     root.wait_window(useCaseForm.top)
